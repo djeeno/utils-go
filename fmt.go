b263af0b820e787e3296a317ff35ed6abb040627
djeeno <djeeeno@gmail.com>
2019-10-29 16:30:20 +0900

    release v0.0.8

--- fmt.go
+++ fmt.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"os"
 )
 
 var Fmt = fmtT{}
@@ -12,4 +13,10 @@ type fmtT struct{}
 // It returns the number of bytes written and any write error encountered.
 func (fmtT) Printfln(format string, a ...interface{}) {
 	fmt.Println(fmt.Sprintf(format, a...))
+}
+
+// Printfln formats according to a format specifier and writes to standard output *with new line*.
+// It returns the number of bytes written and any write error encountered.
+func (fmtT) PrintflnStderr(format string, a ...interface{}) {
+	fmt.Println(fmt.Fprintf(os.Stderr, format, a...))
 }
--- fmt_test.go
+++ fmt_test.go
@@ -1,9 +1,11 @@
 package utils
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestFmtT_Printfln(t *testing.T) {
 	Fmt.Printfln("test: %s", "TestFmtT_Printfln()")
+}
+
+func TestFmtT_PrintflnStderr(t *testing.T) {
+	Fmt.Printfln("test: %s", "TestFmtT_PrintflnStderr()")
 }
