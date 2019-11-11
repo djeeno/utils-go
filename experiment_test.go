a832d1e59b273d7432434e79d728f032ea86efd3
djeeno <djeeeno@gmail.com>
2019-11-11 11:53:25 +0900

    tidy

--- experiment_test.go
+++ experiment_test.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"bytes"
-	"errors"
 	"io"
 	"io/ioutil"
 	"mime/multipart"
@@ -10,46 +9,49 @@ import (
 	"testing"
 )
 
-var testHTTP = Experiment.HTTP
-
 func TestHttpT_PostFormFile(t *testing.T) {
 	t.Helper()
+	testHTTP := Experiment.HTTP
 
 	testReadCloser := ioutil.NopCloser(bytes.NewReader([]byte("testReadCloser")))
 
-	// http.Post / err != nil
-	testHTTP.httpPost = func(url, contentType string, body io.Reader) (resp *http.Response, err error) {
-		return &http.Response{
-			Body: testReadCloser,
-		}, nil
-	} // mock http.Post
-	if _, err := testHTTP.PostFormFile("", "", "", testReadCloser); err != nil {
-		t.Errorf("TestHttpT_PostFormFile(): testHTTP.PostFormFile(): err != nil")
-	}
+	t.Run("normal/http.Post", func(t *testing.T) {
+		testHTTP.httpPost = func(url, contentType string, body io.Reader) (resp *http.Response, err error) {
+			return &http.Response{
+				Body: testReadCloser,
+			}, nil
+		} // mock http.Post
+		if _, err := testHTTP.PostFormFile("", "", "", testReadCloser); err != nil {
+			t.Errorf("TestHttpT_PostFormFile(): testHTTP.PostFormFile(): err != nil")
+		}
+	})
 
-	// http.Post / err == nil
-	testHTTP.httpPost = func(url, contentType string, body io.Reader) (resp *http.Response, err error) {
-		return &http.Response{
-			Body: testReadCloser,
-		}, errors.New("http.Post is mock")
-	} // mock http.Post
-	if _, err := testHTTP.PostFormFile("", "", "", testReadCloser); err == nil {
-		t.Errorf("TestHttpT_PostFormFile(): testHTTP.PostFormFile(): err == nil")
-	}
+	t.Run("non-normal/http.Post", func(t *testing.T) {
+		testHTTP.httpPost = func(url, contentType string, body io.Reader) (resp *http.Response, err error) {
+			return &http.Response{
+				Body: testReadCloser,
+			}, ErrorDummyErrorForTest
+		} // mock http.Post
+		if _, err := testHTTP.PostFormFile("", "", "", testReadCloser); err == nil {
+			t.Errorf("TestHttpT_PostFormFile(): testHTTP.PostFormFile(): err == nil")
+		}
+	})
 
-	// io.Copy / err == nil
-	testHTTP.ioCopy = func(dst io.Writer, src io.Reader) (written int64, err error) {
-		return 0, errors.New("io.Copy is mock")
-	} // mock io.Copy
-	if _, err := testHTTP.PostFormFile("", "", "", testReadCloser); err == nil {
-		t.Errorf("TestHttpT_PostFormFile(): testHTTP.PostFormFile(): err == nil")
-	}
+	t.Run("non-normal/io.Copy", func(t *testing.T) {
+		testHTTP.ioCopy = func(dst io.Writer, src io.Reader) (written int64, err error) {
+			return 0, ErrorDummyErrorForTest
+		} // mock io.Copy
+		if _, err := testHTTP.PostFormFile("", "", "", testReadCloser); err == nil {
+			t.Errorf("TestHttpT_PostFormFile(): testHTTP.PostFormFile(): err == nil")
+		}
+	})
 
-	// multipart.CreateFormFile / err == nil
-	testHTTP.multipartCreateFormFile = func(multipartWriter *multipart.Writer, fieldname, filename string) (io.Writer, error) {
-		return nil, errors.New("multipart.CreateFormFile is mock")
-	} // mock multipart.CreateFormFile
-	if _, err := testHTTP.PostFormFile("", "", "", testReadCloser); err == nil {
-		t.Errorf("TestHttpT_PostFormFile(): testHTTP.PostFormFile(): err == nil")
-	}
+	t.Run("non-normal/multipart.CreateFormFile", func(t *testing.T) {
+		testHTTP.multipartCreateFormFile = func(multipartWriter *multipart.Writer, fieldname, filename string) (io.Writer, error) {
+			return nil, ErrorDummyErrorForTest
+		} // mock multipart.CreateFormFile
+		if _, err := testHTTP.PostFormFile("", "", "", testReadCloser); err == nil {
+			t.Errorf("TestHttpT_PostFormFile(): testHTTP.PostFormFile(): err == nil")
+		}
+	})
 }
